4addcf3818de7701d57db67bfd985be4397e71ba
Juan Alegre <jpalegree@gmail.com>
2022-11-04 06:15:45 -0300

    Add bottom Navigation and Navigation main graph

--- app/src/main/java/com/Alkemy/alkemybankbase/MainActivity.kt
+++ app/src/main/java/com/Alkemy/alkemybankbase/MainActivity.kt
@@ -2,12 +2,52 @@ package com.Alkemy.alkemybankbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.Alkemy.alkemybankbase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        observeDestinationChange(navController)
 
 
+    }
+
+    private fun observeDestinationChange(navController: NavController) {
+        navController.addOnDestinationChangedListener { controller, destination, listener ->
+            when(destination.id){
+                R.id.homeFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.loginFragment -> {
+                    //This visibility is VISIBLE only for now just to verify bottomNav functionality
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.balanceFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.topUpBalanceFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.sendMoneyFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                R.id.transactionsFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
